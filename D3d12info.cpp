dfdd28f7f3e8756935f5f4246197d3a88f9b987b
Dietmar Suoch <dietmar.suoch@intel.com>
2018-10-29 12:21:57 -0700

    removed precompiled header

--- D3d12info.cpp
+++ D3d12info.cpp
@@ -1,5 +1,23 @@
-#include "pch.h"
-#include <iostream>
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <atlbase.h> // for CComPtr
+
+#include <vector>
+#include <string>
+
+#include <cstdint>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#define CHECK_HR(expr)   do { HRESULT hr__ = (expr); if(FAILED(hr__)) assert(0 && #expr); } while(false)
+
+#define SAFE_RELEASE(x)   do { if(x) { (x)->Release(); (x) = nullptr; } } while(false)
+
 
 static const wchar_t* D3D12_SHADER_MIN_PRECISION_SUPPORT_NAMES[] = {
     L"D3D12_SHADER_MIN_PRECISION_SUPPORT_NONE",
